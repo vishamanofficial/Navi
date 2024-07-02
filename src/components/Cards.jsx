b9b9f719348d1f2d493f981ed5825b8fe78fd7e3
Aman Vishwakarma <vishamanofficial@gmail.com>
2024-07-02 13:41:52 +0530

    navi

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,17 +7,17 @@ const Cards = () => {
       <div className='cardcontainer w-1/2 h-[60vh]'>
         <div className='card relative w-full h-full bg-[#004D43] rounded-2xl flex items-center justify-center'>
             <span className='text-[#CDEA68] text-[4vw] font-medium' >navi</span>
-            <button className='absolute left-10 bottom-10 text-[#CDEA68] text-[1vw] px-5 py-1 border-[1px] border-[#CDEA68] rounded-full'>&copy; 2019-2022</button>
+            <button className='absolute left-5 bottom-10 text-[#CDEA68] text-[1vw] px-5 py-1 border-[1px] border-[#CDEA68] rounded-full'>&copy; 2019-2022</button>
         </div>
       </div>
       <div className='cardcontainer w-1/2 h-[60vh] flex gap-5'>
       <div className='card relative w-1/2 h-full bg-[#212121] rounded-2xl flex items-center justify-center'>
-      <img className='w-32' src="src\assets\img\logo002.svg" alt="" />
-      <button className='absolute left-10 bottom-10 text-white text-[1vw] px-5 py-1 border-[1px] border-white rounded-full'>RATING 5.0 ON CLUTCH</button>   
+      <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
+      <button className='absolute left-5 bottom-10 text-white text-[1vw] px-5 py-1 border-[1px] border-white rounded-full'>RATING 5.0 ON CLUTCH</button>   
       </div>
       <div className='card relative w-1/2 h-full bg-[#212121] rounded-2xl flex items-center justify-center'>
-      <img className='w-32' src="src\assets\img\logo003.png" alt="" />
-      <button className='absolute left-10 bottom-10 text-white text-[1vw] px-5 py-1 border-[1px] border-white rounded-full'>BUSINESS BOOTCAMP ALUMNI</button>   
+      <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt="" />
+      <button className='absolute left-5 bottom-10 text-white text-[1vw] px-5 py-1 border-[1px] border-white rounded-full'>BUSINESS BOOTCAMP ALUMNI</button>   
       </div>
      
       </div>
